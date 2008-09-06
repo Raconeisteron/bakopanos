ee707e696468dd29e3d29ade80f9ec02754ba106
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2008-09-06 18:40:28 +0000

    namespace changes

---
+++ trunk/Bakopanos.NW/Application.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Reflection
+
+Class Application
+
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+
+    Const AssemblyFile = "Samples.NW.Services.Dll"
+    Const TypeName = "Bakopanos.NW.Services.Manager"
+
+    Public Sub New()        
+        Dim obj As Object = _
+        Assembly.LoadFrom(AssemblyFile).CreateInstance(TypeName)
+    End Sub
+End Class
