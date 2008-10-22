d66ad3c897928a011391ec600a2efaf97508af65
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2008-10-22 11:29:40 +0000

    NEW PROJECT
    Initial Drop (missing files)

---
+++ Bakopanos/Bakopanos/NW/WinClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bakopanos.NW.WinClient
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Application.Run(new MainForm());
+            
+        }
+    }
+}
