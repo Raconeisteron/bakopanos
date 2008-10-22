004b1a0e23c0faf733b4a35fc9c508ede86cd53e
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2008-10-22 15:23:04 +0000

    NEW PROJECT
    split up projects

--- Bakopanos/Bakopanos.WinClient/ProductsModule/Views/ProductListView.Designer.cs
+++ Bakopanos/Bakopanos.WinClient/ProductsModule/Views/ProductListView.Designer.cs
@@ -35,9 +35,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(172, 115);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(584, 380);
             this.dataGridView1.TabIndex = 1;
             // 
             // ProductListView
