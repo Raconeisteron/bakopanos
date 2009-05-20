d31f91ccd81832f26d08dfbc3cc904e9c8c43237
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2009-05-20 05:37:44 +0000

    old stuff

--- trunk/source/Evolutil.Library.Postsharp/IFirePropertyChanged.cs
+++
@@ -1,9 +0,0 @@
-#pragma warning disable 1591 // Ignore missing comments
-
-namespace Evolutil.Domain.Postsharp
-{
-    public interface IFirePropertyChanged
-    {
-        void OnPropertyChanged(string propertyName);
-    }
-}
--- trunk/source/Evolutil.WebForms/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Evolutil.WebForms.Global" Language="C#" %>
