6ad60429d62f1d1ad3f8ae0317b003ba2ce75e01
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2009-12-06 18:17:58 +0000

    funqunity

--- trunk/architecturesamples/FunqUnity/FunqUnity.Infrastructure.Repository/ContainerConfigurator.cs
+++ trunk/architecturesamples/FunqUnity/FunqUnity.Infrastructure.Repository/ContainerConfigurator.cs
@@ -10,35 +10,60 @@ namespace FunqUnity.Infrastructure.Repository
 
     public class ContainerConfigurator : ContainerConfiguratorBase
     {
+        IUnityContainer _childContainer;
+
         public override IUnityContainer Configure(IUnityContainer container)
         {
             //isolate data layer in child container
-            IUnityContainer childContainer = container.CreateChildContainer();
+            _childContainer = container.CreateChildContainer();
 
             //use lambda to pull services from the child container
-            childContainer.RegisterType<IProductRepository, ProductRepository>(new ContainerControlledLifetimeManager());
-            container.RegisterInstance<Func<IProductRepository>>(() => childContainer.Resolve<IProductRepository>());
+            _childContainer.RegisterType<IProductRepository, ProductRepository>(new ContainerControlledLifetimeManager());
+            container.RegisterInstance<Func<IProductRepository>>(() => _childContainer.Resolve<IProductRepository>());
 
             //deal with db specific technology only here...
-            childContainer.RegisterInstance<Func<IDbConnection>>(() => new SqlCeConnection(ConnectionString));
-            childContainer.RegisterInstance<Func<string, IDbCommand>>(
-                delegate(string cmd)
-                {
-                    IDbConnection connection = childContainer.Resolve<Func<IDbConnection>>()();
-                    connection.Open();
-                    return new SqlCeCommand(cmd, (SqlCeConnection)connection);
-                }
-                );
+            _childContainer.RegisterInstance<Func<IDbConnection>>(() => fConnection());
+            _childContainer.RegisterInstance<Func<string, IDbCommand>>((cmd) => fCommand(cmd));
 
             return container;
+        }
+
+        private IDbConnection fConnection()
+        {
+            switch (ProviderName)
+            {
+                case "SqlCe":
+                    return new SqlCeConnection(ConnectionString);
+                default:
+                    throw new NotSupportedException();
+            }         
+        }
+
+        private IDbCommand fCommand(string cmd)
+        {
+            IDbConnection connection = _childContainer.Resolve<Func<IDbConnection>>()();
+            connection.Open();
+            switch (ProviderName)
+            {
+                case "SqlCe":
+                    return new SqlCeCommand(cmd, (SqlCeConnection)connection);
+                default:
+                    throw new NotSupportedException();
+            }
         }
 
         [ConfigurationProperty("connectionString")]
         public string ConnectionString
         {
-            get { return this["connectionString"] as string; }
-            set { this["connectionString"] = value; }
+            get { return this["connectionString"] as string; }            
         }
 
+
+        [ConfigurationProperty("providerName")]
+        public string ProviderName
+        {
+            get { return this["providerName"] as string; }            
+        }        
+               
     }
 }
