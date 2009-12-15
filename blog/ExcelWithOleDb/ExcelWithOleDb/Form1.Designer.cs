60f95698a6361ab957449d4a08ba4d206950b74d
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2009-12-15 10:53:57 +0000

    oledb with excel

--- blog/ExcelWithOleDb/ExcelWithOleDb/Form1.Designer.cs
+++ blog/ExcelWithOleDb/ExcelWithOleDb/Form1.Designer.cs
@@ -28,27 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonCreateSheet = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonDrop = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonRead
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonRead.Location = new System.Drawing.Point(197, 12);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(116, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(197, 41);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(310, 150);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonCreateSheet
+            // 
+            this.buttonCreateSheet.Location = new System.Drawing.Point(12, 12);
+            this.buttonCreateSheet.Name = "buttonCreateSheet";
+            this.buttonCreateSheet.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreateSheet.TabIndex = 0;
+            this.buttonCreateSheet.Text = "CreateSheet";
+            this.buttonCreateSheet.UseVisualStyleBackColor = true;
+            this.buttonCreateSheet.Click += new System.EventHandler(this.buttonCreateSheet_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(12, 41);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(12, 70);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 0;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(12, 99);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 0;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonDrop
+            // 
+            this.buttonDrop.Location = new System.Drawing.Point(12, 128);
+            this.buttonDrop.Name = "buttonDrop";
+            this.buttonDrop.Size = new System.Drawing.Size(75, 23);
+            this.buttonDrop.TabIndex = 0;
+            this.buttonDrop.Text = "Drop";
+            this.buttonDrop.UseVisualStyleBackColor = true;
+            this.buttonDrop.Click += new System.EventHandler(this.buttonDrop_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(278, 12);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // Form1
             // 
@@ -56,7 +122,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(558, 275);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDrop);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonCreateSheet);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonRead);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,8 +137,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonRead;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonCreateSheet;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonDrop;
+        private System.Windows.Forms.Button buttonSave;
     }
 }
 
--- blog/ExcelWithOleDb/ExcelWithOleDb/Form1.cs
+++ blog/ExcelWithOleDb/ExcelWithOleDb/Form1.cs
@@ -15,29 +15,109 @@ namespace ExcelWithOleDb
     {
         public Form1()
         {
-            InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OleDbConnection connection = new OleDbConnection(GetConnectionString());
-            connection.Open();
-            string cmdText = "Select p.id, fname, lname, jobdesc From [Persons$] as p,[Jobs$] as j Where p.jobid=j.id";
-            OleDbCommand command = new OleDbCommand(cmdText, connection);
-            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
-            DataSet ds = new DataSet();
-            adapter.Fill(ds);
-            dataGridView1.DataSource=ds.Tables[0];
-            connection.Close();
+            InitializeComponent();            
         }
 
         private string _ExcelFilename="template.xls";
 
         public string GetConnectionString() 
        {
+            //if HDR=NO then columns are: F1,F2,... and so on...
            return @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + _ExcelFilename + 
                   @";Extended Properties=""Excel 8.0;HDR=YES;"""; 
-       } 
+       }
+
+        private void buttonCreateSheet_Click(object sender, EventArgs e)
+        {
+           
+            //problems
+            //ALTER TABLE PDC ADD CONSTRAINT INFOindex0 PRIMARY KEY (NAME)
+            //Create table NewTable ( ID int PRIMARY KEY, stringCol nvarchar(255),intCol int )
+            //Create table NewTable ( ID int NOT NULL, stringCol nvarchar(255),intCol int )
+            OleDbConnection connection = new OleDbConnection(GetConnectionString());
+            connection.Open();
+            string cmdText = "Create table NewTable ( ID int, stringCol nvarchar(255),intCol int )";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            OleDbConnection connection = new OleDbConnection(GetConnectionString());
+            connection.Open();
+            string cmdText = "Insert into [NewTable$]( stringCol,intCol) Values(@stringCol,@intCol)";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+
+            command.Parameters.AddWithValue("@stringCol", "a");
+            command.Parameters.AddWithValue("@intCol", 1);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            OleDbConnection connection = new OleDbConnection(GetConnectionString());
+            connection.Open();
+            string cmdText = "Update [NewTable$] Set stringCol= @stringCol, intCol = @intCol";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+
+            command.Parameters.AddWithValue("@stringCol", "a");
+            command.Parameters.AddWithValue("@intCol", 10);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        //problem
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            OleDbConnection connection = new OleDbConnection(GetConnectionString());
+            connection.Open();
+            string cmdText = "Delete from [NewTable$] Where intCol > @intCol";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+
+            command.Parameters.AddWithValue("@intCol", 9);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        //problem: drops only columns but not data or sheet
+        private void buttonDrop_Click(object sender, EventArgs e)
+        {
+            OleDbConnection connection = new OleDbConnection(GetConnectionString());
+            connection.Open();
+            string cmdText = "Drop table NewTable";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+        OleDbDataAdapter adapter;
+        DataSet ds = new DataSet();
+
+        //problem dynamic commands (OleDbCommandBuilder) not supported...
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            new OleDbCommandBuilder(adapter);            
+            adapter.Update(ds);
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            OleDbConnection connection = new OleDbConnection(GetConnectionString());
+            connection.Open();
+            string cmdText = "Select p.id, fname, lname, jobdesc From [Persons$] as p,[Jobs$] as j Where p.jobid=j.id";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+            adapter = new OleDbDataAdapter(command);            
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            connection.Close();
+        } 
 
 
 
