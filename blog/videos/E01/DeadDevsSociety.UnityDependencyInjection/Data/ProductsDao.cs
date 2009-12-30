4ba6ee333aec80810b278941fc85a69474e69134
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2009-12-30 12:11:21 +0000

    episode01

---
+++ blog/videos/E01/DeadDevsSociety.UnityDependencyInjection/Data/ProductsDao.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlServerCe;
+using DeadDevsSociety.UnityDependencyInjection.Business;
+
+namespace DeadDevsSociety.UnityDependencyInjection.Data
+{
+    public class ProductsDao : DataFactory<ProductBo>
+    {
+        public ProductsDao()
+            : base(new SqlCeCommand())
+        {
+            Command.CommandText = "select * from products";
+            string connectionString = ConfigurationManager.AppSettings["connectionstring"];
+            Command.Connection = new SqlCeConnection(connectionString);
+        }
+
+        public override ProductBo Map(IDataRecord record)
+        {
+            var item = new ProductBo
+                           {
+                               FirstName = record["FirstName"] as string,
+                               LastName = record["LastName"] as string
+                           };
+
+            return item;
+        }
+    }
+}
