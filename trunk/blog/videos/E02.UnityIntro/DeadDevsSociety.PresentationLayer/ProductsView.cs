f60c60e9131c5a1ad16e95d3eef823bf2a481779
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-01-22 11:17:43 +0000

    copy for e02

---
+++ trunk/blog/videos/E02.UnityIntro/DeadDevsSociety.PresentationLayer/ProductsView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeadDevsSociety.BusinessLayer;
+using DeadDevsSociety.Framework;
+
+namespace DeadDevsSociety.PresentationLayer
+{
+    public class ProductsView
+    {
+        private ProductsFacade _facade = new ProductsFacade();
+        
+        public void Show()
+        {          
+            LogServiceSingleton.LogService.WriteLine("presentation:show");
+            foreach (Product item in _facade.GetProductByName("."))//all for now...
+            {
+                Console.WriteLine("{0}: {1}",item.Id,item.ProductName);
+            }
+        }
+    }
+}
