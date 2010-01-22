b58396e2925a40c6054616ad6dc1b955f6b62ab5
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-01-22 12:32:38 +0000

    Episode 03 ...

---
+++ trunk/blog/videos/E03.UnityIntro/DeadDevsSociety.PresentationLayer/ProductsView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeadDevsSociety.BusinessLayer;
+using DeadDevsSociety.Framework;
+using Microsoft.Practices.Unity;
+
+namespace DeadDevsSociety.PresentationLayer
+{
+    public interface IProductsView
+    {
+    }
+
+    internal class ProductsView : IProductsView
+    {
+        private readonly IProductsFacade _facade;
+        private readonly LogService _logService;       
+
+        public ProductsView(IProductsFacade facade, LogService logService)
+        {
+            _facade = facade;
+            _logService = logService;
+        }
+        
+        [InjectionMethod]
+        public void Show()
+        {          
+            _logService.WriteLine("presentation:show");
+            foreach (Product item in _facade.GetProductByName("."))//all for now...
+            {
+                Console.WriteLine("{0}: {1}",item.Id,item.ProductName);
+            }
+        }
+    }
+}
