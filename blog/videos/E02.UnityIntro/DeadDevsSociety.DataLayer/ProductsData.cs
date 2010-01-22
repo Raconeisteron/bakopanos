977525cc241fbd7599759d19fdba962b767b333e
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-01-22 11:17:43 +0000

    copy for e02

---
+++ blog/videos/E02.UnityIntro/DeadDevsSociety.DataLayer/ProductsData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.Linq;
+using System.Text;
+using DeadDevsSociety.Framework;
+
+namespace DeadDevsSociety.DataLayer
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+
+    public class ProductsData
+    {
+        private const string ConnectionString = "Data Source=UnityIntro.sdf";
+        private readonly SqlCeConnection _connection = new SqlCeConnection(ConnectionString);
+
+        public IList<ProductEntity> GetAllProducts()
+        {
+            LogServiceSingleton.LogService.WriteLine("data:get");
+
+            IList<ProductEntity> list = new List<ProductEntity>();
+            _connection.Open();
+
+            var command = new SqlCeCommand("select * from products", _connection);
+            SqlCeDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (reader.Read())
+            {
+                list.Add(new ProductEntity
+                             {
+                                 Id = Convert.ToInt32(reader["Id"]), 
+                                 ProductName = reader["ProductName"] as string
+                             });
+            }
+
+            return list;
+        }
+    }
+}
