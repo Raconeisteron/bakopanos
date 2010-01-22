a3917746bdb197d1f955cad3b9d36ea80093a817
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-01-22 11:12:39 +0000

    Episode 01 ...

---
+++ blog/videos/E01.UnityIntro/DeadDevsSociety.UnityIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using DeadDevsSociety.PresentationLayer;
+
+namespace DeadDevsSociety.UnityIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            new ProductsView().Show();
+
+            Console.ReadLine();
+        }
+    }
+}
