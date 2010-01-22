83b6ac16dddbd6a1b2554c2737d151ab8e0c7965
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-01-22 13:02:06 +0000

    Episode 03 ...

---
+++ trunk/blog/videos/E03.UnityIntro/DeadDevsSociety.BusinessLayer/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using DeadDevsSociety.Framework;
+using Microsoft.Practices.Unity;
+
+namespace DeadDevsSociety.BusinessLayer
+{
+    public class Module : ConfigurationSection//,IModule
+    {
+        public IUnityContainer Configure(IUnityContainer container)
+        {
+            container.RegisterType<IProductsFacade,ProductsFacade>();            
+            return container;
+        }
+    }
+}
---
+++ trunk/blog/videos/E03.UnityIntro/DeadDevsSociety.PresentationLayer/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using DeadDevsSociety.Framework;
+using Microsoft.Practices.Unity;
+
+namespace DeadDevsSociety.PresentationLayer
+{
+    public class Module : ConfigurationSection//, IModule
+    {
+        public IUnityContainer Configure(IUnityContainer container)
+        {
+            container.RegisterType<IProductsView,ProductsView>();            
+            return container;
+        }
+    }
+}
