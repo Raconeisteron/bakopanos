7b377ae8d33566ecb1dc591b3d3091f487563def
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-01-22 13:03:12 +0000

    Episode 04 ...

---
+++ trunk/blog/videos/E04.UnityIntro/DeadDevsSociety.Framework/IModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace DeadDevsSociety.Framework
+{
+    //public interface IModule
+    //{
+    //    IUnityContainer Configure(IUnityContainer container);
+    //}
+}
