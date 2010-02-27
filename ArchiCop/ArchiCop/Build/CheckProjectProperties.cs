9f99b00fc897bfc2d77dbb28ed327693ca63a916
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-02-27 22:15:40 +0000

    experimental version of archicop... work in progress

---
+++ ArchiCop/ArchiCop/Build/CheckProjectProperties.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.Text.RegularExpressions;
+using System.Data.OleDb;
+using System.Data;
+
+namespace ArchiCop.Build
+{
+    public class CheckProjectProperties : CheckVisualStudioProjectList
+    {
+        [Required]
+        public string ConnectionString { get; set; }
+        
+        [Required]
+        public string ProviderName { get; set; }
+
+        [Required]
+        public string Table { get; set; }
+
+        public override void CheckProject(VisualStudioProject project)
+        {            
+            DataSet ds = new DataSet(); 
+
+            OleDbConnection connection = new OleDbConnection(ConnectionString);
+            connection.Open();
+            string cmdText = "Select PropertyName, PropertyPattern From [" + Table + "$]";
+            OleDbCommand command = new OleDbCommand(cmdText, connection);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds);            
+            connection.Close();
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                string name = row["PropertyName"] as string;
+                string pattern = row["PropertyPattern"] as string;
+
+                string value = DataMapper.GetPropertyValue(project, name).ToString();
+                bool retValue = Regex.IsMatch(value, pattern);
+                if (retValue == false)
+                {
+                    Log.LogError("{0} doesn't match regex pattern {1}", name, pattern);
+                }                      
+            }            
+        }
+
+    }
+}
---
+++ ArchiCop/ArchiCop/DataMapper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using System.Data;
+using System.Reflection;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace ArchiCop
+{   
+    /// <summary>
+    /// Map data from a source into a target object
+    /// by copying public property values.
+    /// </summary>
+    /// <remarks></remarks>
+    public static class DataMapper
+    {
+        /// <summary>
+        /// Gets an object's property value        
+        /// </summary>
+        /// <param name="target">Object containing the property to get.</param>
+        /// <param name="propertyName">Name of the property to set.</param>        
+        public static object GetPropertyValue(object source, string propertyName)
+        {
+            PropertyInfo propertyInfo =
+                source.GetType().GetProperty(propertyName);
+            return propertyInfo.GetValue(source, new object[] { });
+        }
+
+        /// <summary>
+        /// Sets an object's property with the specified value,
+        /// coercing that value to the appropriate type if possible.
+        /// </summary>
+        /// <param name="target">Object containing the property to set.</param>
+        /// <param name="propertyName">Name of the property to set.</param>
+        /// <param name="value">Value to set into the property.</param>
+        public static void SetPropertyValue(
+            object target, string propertyName, object value)
+        {
+            PropertyInfo propertyInfo =
+                target.GetType().GetProperty(propertyName);
+            if (value == null)
+                propertyInfo.SetValue(target, value, null);
+            else
+            {
+                Type pType =
+                    GetPropertyType(propertyInfo.PropertyType);
+                Type vType =
+                    GetPropertyType(value.GetType());
+                if (pType.Equals(vType))
+                {
+                    // types match, just copy value
+                    propertyInfo.SetValue(target, value, null);
+                }
+                else
+                {
+                    // types don't match, try to coerce
+                    if (pType.Equals(typeof(Guid)))
+                        propertyInfo.SetValue(
+                            target, new Guid(value.ToString()), null);
+                    else if (pType.IsEnum && vType.Equals(typeof(string)))
+                        propertyInfo.SetValue(target, Enum.Parse(pType, value.ToString()), null);
+                    else
+                        propertyInfo.SetValue(
+                            target, Convert.ChangeType(value, pType), null);
+                }
+            }
+        }
+
+        public static PropertyInfo[] GetSourceProperties(Type sourceType)
+        {
+            List<PropertyInfo> result = new List<PropertyInfo>();
+            PropertyDescriptorCollection props =
+                TypeDescriptor.GetProperties(sourceType);
+            foreach (PropertyDescriptor item in props)
+                if (item.IsBrowsable)
+                    result.Add(sourceType.GetProperty(item.Name));
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Returns a property's type, dealing with
+        /// Nullable(Of T) if necessary.
+        /// </summary>
+        /// <param name="propertyType">Type of the
+        /// property as returned by reflection.</param>
+        private static Type GetPropertyType(Type propertyType)
+        {
+            Type type = propertyType;
+            if (type.IsGenericType &&
+                (type.GetGenericTypeDefinition() == typeof(Nullable<>)))
+                return Nullable.GetUnderlyingType(type);
+            return type;
+        }
+    }
+}
