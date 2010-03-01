18990496c68fd32396fae4db024e4a70cc8d6e44
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-03-01 14:37:29 +0000

    enabled rules... prototype archicop....

--- trunk/ArchiCop/ArchiCop.Rules/CheckProjectPropertiesRule.cs
+++ trunk/ArchiCop/ArchiCop.Rules/CheckProjectPropertiesRule.cs
@@ -1,31 +1,105 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace ArchiCop.Rules
 {
     public class CheckProjectPropertiesRule : ArchiCopRule
-    {        
-        private List<ProjectPropertyRule> _rules;
+    {
+        private List<ProjectProperty> _rules;
 
         public override void Init(string table, string providerName, string connectionString)
         {
-            string cmdText = "Select PropertyName, PropertyPattern From " + table + "";
-            _rules = DataHelper.GetData<ProjectPropertyRule>(providerName, connectionString, cmdText);
+            string cmdText = "Select PropertyName, PropertyPattern, RuleType, Message From " + table + "";
+            _rules = DataHelper.GetData<ProjectProperty>(providerName, connectionString, cmdText);
         }
 
-        public override void CheckProject(VisualStudioProject project)                    
-        {            
-            foreach (ProjectPropertyRule rule in _rules)
+        public override void CheckProject(VisualStudioProject project)
+        {
+            var propertyNames = (from item in _rules
+                                 select item.PropertyName).Distinct();
+
+            foreach (string propertyName in propertyNames)
             {
-                string nameValue = DataMapper.GetPropertyValue(project, rule.PropertyName).ToString();
-                
-                bool retValue = Regex.IsMatch(nameValue, rule.PropertyPattern);
-                if (retValue == false)
+                string propertyNameValue = DataMapper.GetPropertyValue(project, propertyName).ToString();
+
+                var exclude = from item in _rules
+                              where item.PropertyName == propertyName
+                              where item.RuleType == ArchiCopRuleType.Exclude
+                              select item;
+
+                var valid = from item in _rules
+                            where item.PropertyName == propertyName
+                            where item.RuleType == ArchiCopRuleType.Valid
+                            select item;
+
+                var warning = from item in _rules
+                              where item.PropertyName == propertyName
+                              where item.RuleType == ArchiCopRuleType.Warning
+                              select item;
+
+                var invalid = from item in _rules
+                              where item.PropertyName == propertyName
+                              where item.RuleType == ArchiCopRuleType.Invalid
+                              select item;
+
+                //step 1
+                bool isExclude = false;
+                foreach (ProjectProperty rule in exclude)
                 {
-                    Log.LogError("{0} doesn't match regex pattern {1}", rule.PropertyName, rule.PropertyPattern);
+                    bool retValue = Regex.IsMatch(propertyNameValue, rule.PropertyPattern);
+                    if (retValue)
+                    {
+                        isExclude = true;
+                        rule.Log(Log, propertyNameValue);                        
+                    }
+                }
+
+                //step 2
+                bool isValid = false;
+                if (isExclude == false)
+                {
+                    foreach (ProjectProperty rule in valid)
+                    {
+                        bool retValue = Regex.IsMatch(propertyNameValue, rule.PropertyPattern);
+                        if (retValue)
+                        {
+                            isValid = true;
+                            rule.Log(Log, propertyNameValue);                        
+                        }
+                    }
+                }
+
+                //step 3
+                bool isWarning = false;
+                if (isValid == false && isExclude == false)
+                {
+                    foreach (ProjectProperty rule in warning)
+                    {
+                        bool retValue = Regex.IsMatch(propertyNameValue, rule.PropertyPattern);
+                        if (retValue)
+                        {
+                            rule.Log(Log, propertyNameValue);                        
+                            isWarning = true;
+                        }
+                    }
+                }
+
+                //step 4
+                if (isValid == false && isWarning == false && isExclude == false)
+                {
+                    foreach (ProjectProperty rule in invalid)
+                    {
+                        bool retValue = Regex.IsMatch(propertyNameValue, rule.PropertyPattern);
+                        if (retValue)
+                        {
+                            rule.Log(Log, propertyNameValue);                        
+                        }
+                    }
                 }
             }
-        }
 
+
+        }        
     }
 }
---
+++ trunk/ArchiCop/ArchiCop.Rules/LogHelpers.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace ArchiCop.Rules
+{
+    public static class LogHelpers
+    {
+        public static void Log(this ProjectProperty rule, TaskLoggingHelper loggingHelper, object propertyNameValue)
+        {
+            string message = string.Format(rule.Message, rule.RuleType, rule.PropertyName, propertyNameValue);
+            if (message.Length>0)
+            {
+                switch (rule.RuleType)
+                {
+                    case ArchiCopRuleType.Exclude:
+                        loggingHelper.LogMessage(message);
+                        break;
+                    case ArchiCopRuleType.Invalid:
+                        loggingHelper.LogError(message);
+                        break;
+                    case ArchiCopRuleType.Valid:
+                        loggingHelper.LogMessage(message);
+                        break;
+                    case ArchiCopRuleType.Warning:
+                        loggingHelper.LogWarning(message);
+                        break;
+                }                
+            }            
+        }
+    }
+    
+}
