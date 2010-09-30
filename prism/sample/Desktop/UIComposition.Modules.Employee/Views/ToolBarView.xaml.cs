44f47f05d19ad8d0ed93262f58401bec364d0318
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-09-30 07:10:01 +0000

    fixes in toolbar

---
+++ prism/sample/Desktop/UIComposition.Modules.Employee/Views/ToolBarView.xaml.cs
@@ -0,0 +1,21 @@
+﻿// ===================================================================================
+// Bakopanos Konstantinos
+// http://www.deaddevssociety.com
+// ===================================================================================
+using System.Windows.Controls;
+
+namespace UIComposition.Modules.Employee.Views
+{
+    /// <summary>
+    ///   Interaction logic for ToolBarView.xaml
+    /// </summary>
+    public partial class ToolBarView : UserControl
+    {
+        public ToolBarView(ToolBarViewModel viewModel)
+        {
+            InitializeComponent();
+
+            DataContext = viewModel;
+        }
+    }
+}
