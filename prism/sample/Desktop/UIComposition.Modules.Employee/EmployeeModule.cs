0a5fc17b40aa08279bf90b40c87a2c03450fd78b
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-09-30 08:51:09 +0000

    small fixes

--- prism/sample/Desktop/UIComposition.Modules.Employee/EmployeeModule.cs
+++ prism/sample/Desktop/UIComposition.Modules.Employee/EmployeeModule.cs
@@ -3,18 +3,23 @@
 // http://www.deaddevssociety.com
 // ===================================================================================
 using Microsoft.Practices.Composite.Modularity;
+using Microsoft.Practices.Composite.Regions;
 using Microsoft.Practices.Unity;
 using UIComposition.Modules.Employee.Controllers;
+using UIComposition.Modules.Employee.Views;
 
 namespace UIComposition.Modules.Employee
 {
     public class EmployeeModule : IModule
     {
         private readonly IUnityContainer _unityContainer;
+        private IRegionViewRegistry _regionViewRegistry;
 
-        public EmployeeModule(IUnityContainer unityContainer)
+        public EmployeeModule(IUnityContainer unityContainer,IRegionViewRegistry regionViewRegistry)
         {
             _unityContainer = unityContainer;
+            _regionViewRegistry = regionViewRegistry;
+
         }
 
         #region IModule Members
@@ -22,8 +27,13 @@ namespace UIComposition.Modules.Employee
         public void Initialize()
         {
             _unityContainer.RegisterSingleton<IEmployeesController, EmployeesController>();
-
             _unityContainer.Resolve<IEmployeesController>();
+
+            _regionViewRegistry.RegisterViewWithRegion(RegionNames.ToolBarRegion,
+                                                       () =>
+                                                       new ToolBarView(
+                                                           _unityContainer.Resolve<ToolBarViewModel>()));
+            
         }
 
         #endregion
