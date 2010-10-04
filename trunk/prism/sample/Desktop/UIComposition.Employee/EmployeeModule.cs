6e276117435708ee17fd33e1bd04903fa0140cbc
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-10-04 18:56:20 +0000

    fixed model a bit

--- trunk/prism/sample/Desktop/UIComposition.Employee/EmployeeModule.cs
+++ trunk/prism/sample/Desktop/UIComposition.Employee/EmployeeModule.cs
@@ -3,42 +3,38 @@
 // http://www.deaddevssociety.com
 // ===================================================================================
 using Microsoft.Practices.Composite.Modularity;
-using Microsoft.Practices.Composite.Regions;
 using Microsoft.Practices.Unity;
 using UIComposition.Infrastructure;
 using UIComposition.Employee.Controllers;
 using UIComposition.Employee.Model;
 using UIComposition.Employee.Views;
 using UIComposition.Model;
-using UIComposition.Services;
 
 namespace UIComposition.Employee
 {
     public class EmployeeModule : IModule
     {
         private readonly IUnityContainer _unityContainer;
-        private readonly IRegionViewRegistry _regionViewRegistry;
 
-        public EmployeeModule(IUnityContainer unityContainer,IRegionViewRegistry regionViewRegistry)
+        public EmployeeModule(IUnityContainer unityContainer)
         {
             _unityContainer = unityContainer;
-            _regionViewRegistry = regionViewRegistry;
-
         }
 
         #region IModule Members
 
         public void Initialize()
         {
+            _unityContainer.RegisterSingleton<EmployeeWorkItem, EmployeeWorkItem>();
             _unityContainer.RegisterSingleton<IEmployeeWorkItem, EmployeeWorkItem>();
+            _unityContainer.RegisterSingleton<IProjectList, EmployeeWorkItem>();
+            _unityContainer.RegisterSingleton<IEmployeeList, EmployeeWorkItem>();
+            _unityContainer.RegisterSingleton<IEmployeeInfo, EmployeeWorkItem>();
 
             _unityContainer.RegisterSingleton<IEmployeesController, EmployeesController>();
             _unityContainer.Resolve<IEmployeesController>();
 
-            _regionViewRegistry.RegisterViewWithRegion(RegionNames.ToolBarRegion,
-                                                       () =>
-                                                       new ToolBarView(
-                                                           _unityContainer.Resolve<ToolBarViewModel>()));
+            _unityContainer.RegisterViewWithRegion<ToolBarView>(RegionNames.ToolBarRegion);
             
         }
 
--- trunk/prism/sample/Desktop/UIComposition.Project/ProjectModule.cs
+++ trunk/prism/sample/Desktop/UIComposition.Project/ProjectModule.cs
@@ -3,27 +3,21 @@
 // http://www.deaddevssociety.com
 // ===================================================================================
 using Microsoft.Practices.Composite.Modularity;
-using Microsoft.Practices.Composite.Regions;
 using Microsoft.Practices.Unity;
 using UIComposition.Infrastructure;
 using UIComposition.Model;
 using UIComposition.Project.Model;
 using UIComposition.Project.Views;
-using UIComposition.Services;
 
 namespace UIComposition.Project
 {
     public class ProjectModule : IModule
     {
-        private readonly IRegionViewRegistry _regionViewRegistry;
-        private readonly IEmployeeWorkItem _employeeWorkItem;
+       
         private readonly IUnityContainer _unityContainer;
 
-        public ProjectModule(IEmployeeWorkItem employeeWorkItem,
-            IRegionViewRegistry regionViewRegistry,IUnityContainer unityContainer)
+        public ProjectModule(IUnityContainer unityContainer)
         {
-            _employeeWorkItem = employeeWorkItem;            
-            _regionViewRegistry = regionViewRegistry;
             _unityContainer = unityContainer;
         }
 
@@ -33,16 +27,13 @@ namespace UIComposition.Project
         {
             //this is an optional module. Isolate it...
             IUnityContainer container= _unityContainer.CreateChildContainer();
-            container.RegisterSingleton<IProjectWorkItem, ProjectWorkItem>();     
+            container.RegisterSingleton<ProjectWorkItem, ProjectWorkItem>();
+            container.RegisterSingleton<IProjectWorkItem, ProjectWorkItem>();
+            container.RegisterSingleton<IProjectList, ProjectWorkItem>();     
 
             // Register a type for pull based based composition. 
-            _regionViewRegistry.RegisterViewWithRegion(RegionNames.NaviRegion,
-                                                       () =>
-                                                       new ProjectsListView(new ProjectsListViewModel(container.Resolve<IProjectWorkItem>())));
-
-            _regionViewRegistry.RegisterViewWithRegion(RegionNames.MainDetailsTabRegion,
-                                                       () =>
-                                                       new ProjectsListView(new ProjectsListViewModel(_employeeWorkItem)));
+            _unityContainer.RegisterViewWithRegion<ProjectsListView>(RegionNames.NaviRegion);
+            _unityContainer.RegisterViewWithRegion<ProjectsListView>(RegionNames.MainDetailsTabRegion);
         }
 
         #endregion
