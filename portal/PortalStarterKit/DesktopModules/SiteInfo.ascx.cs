5506d013c6fe34f5b1a7a0b5b98abea6983de1f5
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-10-15 07:26:22 +0000

    small change

---
+++ portal/PortalStarterKit/DesktopModules/SiteInfo.ascx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ASPNET.StarterKit.Portal;
+using Microsoft.Practices.Unity;
+
+namespace PortalStarterKit.DesktopModules
+{
+    public partial class SiteInfo : PortalModuleControl<SiteInfo>
+    {
+        [Dependency]
+        public ISiteConfigurationService ConfigurationService
+        {
+            private
+            get;
+            set;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                tabList.DataSource = ConfigurationService.ActivePortal(0).DesktopTabs;                
+                tabList.DataBind();
+            }
+            
+        }
+    }
+}
