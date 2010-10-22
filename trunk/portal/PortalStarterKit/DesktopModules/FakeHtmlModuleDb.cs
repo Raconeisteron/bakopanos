719115d8f7cc58690c0c3ca0cfa3df4311568552
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-10-22 16:48:12 +0000

    fixed bug

--- trunk/portal/PortalStarterKit/DesktopModules/FakeHtmlModuleDb.cs
+++ trunk/portal/PortalStarterKit/DesktopModules/FakeHtmlModuleDb.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using PortalStarterKit.Core;
 
 namespace PortalStarterKit.DesktopModules
 {
     public class FakeHtmlModuleDb : IHtmlModuleDb
     {
+        private ISiteEnvironment _environment;
+        public FakeHtmlModuleDb(ISiteEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         public string GetHtmlText(string portalId, string tabId, string moduleId)
         {
-            string path = Path.Combine(@"C:\google\google.portal\PortalStarterKit", "test.htm");
+            string path = Path.Combine(_environment.DataPhysicalPath, portalId+@"\"+tabId+@"\"+moduleId);
+            path = Path.Combine(path, "test.htm");
             string htmlText;
             // create reader & open file
             using (TextReader tr = new StreamReader(path))
