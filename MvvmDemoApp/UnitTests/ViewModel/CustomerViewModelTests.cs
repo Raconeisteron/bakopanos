a521fe2202072c1808a73eeb6860c5d8518b9161
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-11-08 08:57:26 +0000

    rework on tests

--- MvvmDemoApp/UnitTests/ViewModel/CustomerViewModelTests.cs
+++ MvvmDemoApp/UnitTests/ViewModel/CustomerViewModelTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
+using DemoApp.DataAccess;
 using DemoApp.DataAccess.Fake;
 using DemoApp.Model;
 using DemoApp.Properties;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
 
 namespace DemoApp.ViewModel
 {
@@ -18,37 +20,15 @@ namespace DemoApp.ViewModel
         ///</summary>
         public TestContext TestContext { get; set; }
 
-        #region Additional test attributes
-
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-
-        #endregion
 
         #endregion // Boilerplate Code
 
         [TestMethod]
         public void TestCustomerType()
         {
+            var mocks = new MockRepository();
             Customer cust = Customer.CreateNewCustomer();
-            var repos = new CustomerRepository(Constants.CUSTOMER_DATA_FILE);
+            var repos = mocks.DynamicMock<ICustomerRepository>();
             var target = new CustomerViewModel(cust, repos);
 
             target.CustomerType = Strings.CustomerViewModel_CustomerTypeOption_Company;
