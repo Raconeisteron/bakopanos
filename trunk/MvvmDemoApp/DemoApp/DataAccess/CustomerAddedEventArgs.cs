df40cd414d7ad567c42e797d05ca059cb5085ddb
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-11-26 20:42:36 +0000

    initial unchanged sample, migrated and cleaned up...

---
+++ trunk/MvvmDemoApp/DemoApp/DataAccess/CustomerAddedEventArgs.cs
@@ -0,0 +1,18 @@
+using System;
+using DemoApp.Model;
+
+namespace DemoApp.DataAccess
+{
+    /// <summary>
+    ///   Event arguments used by CustomerRepository's CustomerAdded event.
+    /// </summary>
+    public class CustomerAddedEventArgs : EventArgs
+    {
+        public CustomerAddedEventArgs(Customer newCustomer)
+        {
+            NewCustomer = newCustomer;
+        }
+
+        public Customer NewCustomer { get; private set; }
+    }
+}
