bf058fc8398941fab6a1d222b018e1432e5ea1ac
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-11-28 20:17:19 +0000

    new sample

---
+++ MvvmDemoApp/DemoApp/BuildStarterModule/View/BuildTargetView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DemoApp.View
+{
+    /// <summary>
+    /// Interaction logic for BuildTargetView.xaml
+    /// </summary>
+    public partial class BuildTargetView : UserControl
+    {
+        public BuildTargetView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MvvmDemoApp/DemoApp/BuildStarterModule/ViewModel/BuildTargetViewModel.cs
@@ -0,0 +1,56 @@
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using DemoApp.DataAccess;
+using DemoApp.Model;
+using DemoApp.Properties;
+
+namespace DemoApp.ViewModel
+{
+    /// <summary>
+    ///   A UI-friendly wrapper for a Customer object.
+    /// </summary>
+    public class BuildTargetViewModel : WorkspaceViewModel
+    {
+        #region Fields
+
+       
+
+        #endregion // Fields
+
+        #region Constructor
+
+        public BuildTargetViewModel()
+        {
+            
+        }
+
+        #endregion // Constructor
+
+        #region Customer Properties
+
+        
+
+        #endregion // Customer Properties
+
+        #region Presentation Properties
+
+        
+       
+
+        #endregion // Presentation Properties
+
+        #region Public Methods
+
+        
+        #endregion // Public Methods
+
+        #region Private Helpers
+
+       
+
+        #endregion // Private Helpers
+
+       
+    }
+}
