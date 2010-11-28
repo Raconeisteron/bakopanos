945e5cca49a47f2b007d47a48332a9d68d448c3f
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-11-28 20:17:19 +0000

    new sample

---
+++ trunk/MvvmDemoApp/DemoApp/BuildStarterModule/BuildStarterModule.cs
@@ -0,0 +1,45 @@
+using System.Configuration;
+using System.Linq;
+using DemoApp.DataAccess;
+using DemoApp.Model;
+using DemoApp.Properties;
+using DemoApp.ViewModel;
+using Microsoft.Practices.Unity;
+
+namespace DemoApp
+{
+    public class BuildStarterModule : ConfigurationSection, IModule
+    {
+        #region Fields
+
+        private IUnityContainer _container;
+        private WorkspaceController _workspaces;
+
+        #endregion // Fields
+
+
+        #region IModule Members
+
+        public void Initialize(IUnityContainer container)
+        {
+            _container = container;
+
+            _workspaces = container.Resolve<WorkspaceController>();
+            var commands = container.Resolve<CommandController>();
+
+            commands.Add(new CommandViewModel(
+                             "BuilTarget",
+                             new RelayCommand(param => ShowBuildTarget())));
+        }
+
+        #endregion
+
+        private void ShowBuildTarget()
+        {
+            
+            var workspace = new BuildTargetViewModel();
+            _workspaces.Add(workspace);
+            _workspaces.SetActiveWorkspace(workspace);
+        }
+    }
+}
