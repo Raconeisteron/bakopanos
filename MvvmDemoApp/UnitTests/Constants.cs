0ae05a2f1006019c0bc40f70c03e6be9717e161a
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-11-28 17:34:56 +0000

    structure of data files in tests project

--- MvvmDemoApp/UnitTests/Constants.cs
+++ MvvmDemoApp/UnitTests/Constants.cs
@@ -2,6 +2,6 @@
 {
     internal static class Constants
     {
-        public const string CUSTOMER_DATA_FILE = "/UnitTests;component/customers.xml";
+        public const string CUSTOMER_DATA_FILE = "/UnitTests;component/Data/customers.xml";
     }
 }
