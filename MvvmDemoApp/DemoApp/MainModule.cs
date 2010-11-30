0f30d0d409a6dc5ca294b4b0488337ccc59bb992
cbakopanos <cbakopanos@6473d560-3351-0410-9877-919fe1dee291>
2010-11-30 10:41:16 +0000

    final clean up...

---
+++ MvvmDemoApp/DemoApp/MainModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace DemoApp
+{
+    public class MainModule : IModule
+    {
+        public void Initialize(IUnityContainer container)
+        {
+
+        }
+    }
+}
