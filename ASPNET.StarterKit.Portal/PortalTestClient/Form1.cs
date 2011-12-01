c3f57da16664186a4a259c560d4ef70fb4d1c25a
cbakopanos@gmail.com <cbakopanos@gmail.com@6473d560-3351-0410-9877-919fe1dee291>
2011-12-01 07:49:57 +0000

    testclient

---
+++ ASPNET.StarterKit.Portal/PortalTestClient/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Portal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButtonContacts_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButtonLinks_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButtonAnnouncements_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
