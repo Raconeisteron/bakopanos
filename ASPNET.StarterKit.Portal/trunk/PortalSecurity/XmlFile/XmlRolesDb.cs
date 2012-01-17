043fca78bb84ffcf0e4594bb7e17a6edcd35e846
cbakopanos@gmail.com <cbakopanos@gmail.com@6473d560-3351-0410-9877-919fe1dee291>
2012-01-17 21:28:10 +0000

    XmlFile/XmlRolesDb
    started with xml implementation

--- ASPNET.StarterKit.Portal/trunk/PortalSecurity/XmlFile/XmlRolesDb.cs
+++ ASPNET.StarterKit.Portal/trunk/PortalSecurity/XmlFile/XmlRolesDb.cs
@@ -1,14 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using System.Web;
 
 namespace ASPNET.StarterKit.Portal.XmlFile
 {
     public class XmlRolesDb : IRolesDb
     {
+        private readonly SecurityDataSet _dataSet = new SecurityDataSet();
+
+        public XmlRolesDb(string fileName)
+        {
+            // Retrieve the location of the XML configuration file
+            fileName = HttpContext.Current.Server.MapPath(fileName);
+            _dataSet.ReadXml(fileName);
+        }
+
         public List<PortalRole> GetPortalRoles(int portalId)
         {
-            throw new NotImplementedException();
+
+            return _dataSet.PortalRoles.Select(item=> new PortalRole
+                                                          {
+                                                              PortalId = portalId,
+                                                              RoleId = item.RoleID,
+                                                              RoleName = item.RoleName
+                                                          }).ToList();
         }
 
         public int AddRole(int portalId, string roleName)
