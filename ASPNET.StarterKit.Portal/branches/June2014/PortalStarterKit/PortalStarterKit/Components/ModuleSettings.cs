345ae5f78c50cf020832cbf1302fe5a80bf8980b
cbakopanos@gmail.com <cbakopanos@gmail.com@6473d560-3351-0410-9877-919fe1dee291>
2014-06-14 09:13:40 +0000

    move classes to files

---
+++ ASPNET.StarterKit.Portal/branches/June2014/PortalStarterKit/PortalStarterKit/Components/ModuleSettings.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace ASPNET.StarterKit.Portal
+{
+    //*********************************************************************
+    //
+    // ModuleSettings Class
+    //
+    // Class that encapsulates the detailed settings for a specific Tab 
+    // in the Portal.  ModuleSettings implements the IComparable interface 
+    // so that an ArrayList of ModuleSettings objects may be sorted by
+    // ModuleOrder, using the ArrayList's Sort() method.
+    //
+    //*********************************************************************
+    public class ModuleSettings : IComparable 
+    {
+
+        public int          ModuleId;
+        public int          ModuleDefId;
+        public int          TabId;
+        public int          CacheTime;
+        public int          ModuleOrder;
+        public String       PaneName;
+        public String       ModuleTitle;
+        public String       AuthorizedEditRoles;
+        public bool         ShowMobile;
+        public String       DesktopSrc;
+        public String       MobileSrc;
+
+
+        public int CompareTo(object value) 
+        {
+
+            if (value == null) return 1;
+
+            int compareOrder = ((ModuleSettings)value).ModuleOrder;
+            
+            if (this.ModuleOrder == compareOrder) return 0;
+            if (this.ModuleOrder < compareOrder) return -1;
+            if (this.ModuleOrder > compareOrder) return 1;
+            return 0;
+        }
+    }
+}
